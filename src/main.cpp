1419c62e11be083aad80f7dfd4dd8f4eab8d2873
annickls <129967947+annickls@users.noreply.github.com>
2023-06-06 17:20:50 +0200

    Redesign Board

--- src/main.cpp
+++ src/main.cpp
@@ -28,10 +28,10 @@ class MyCallbacks : public BLECharacteristicCallbacks {
 #define muxS2 27
 #define muxS3 26
 // inout pins Kraftsensoren
-#define force1 25
-#define force2 33
+#define force2 35
 #define force3 32
-#define force4 35
+#define force4 33
+#define force5 25
 
 // Variablendeklaration
 int potentiometerValue = 0;
@@ -98,53 +98,52 @@ void setup() {
     pinMode(muxS2, OUTPUT);
     pinMode(muxS3, OUTPUT);
     // input pins Kraftsensoren
-    pinMode(force1, INPUT);
     pinMode(force2, INPUT);
     pinMode(force3, INPUT);
     pinMode(force4, INPUT);
+    pinMode(force5, INPUT);
 
     Serial.begin(9600);
     
 
 // initialize servos
     Wire.begin();
-    // myServos.begin();
-    // myServos.setPWMFreq(50);
-    // delay(10);
-
+    myServos.begin();
+    myServos.setPWMFreq(50);
+    delay(10);
 
   // initialize IMU
-    if (!mpu.begin()) {
-    Serial.println("Sensor init failed");
-    while (1)
-        yield();
-        }
-    Serial.println("MPU6050 Found!");
+    // if (!mpu.begin()) {
+    // Serial.println("Sensor init failed");
+    // while (1)
+    //     yield();
+    //     }
+    // Serial.println("MPU6050 Found!");
 
-    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-    mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    // mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    // mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    // mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
 
 
 // initialize BLE
-    BLEDevice::init("ESP32 Sensor Export");
-    pServer = BLEDevice::createServer();
-    BLEService *pService = pServer->createService(SERVICE_UUID);
-    pCharacteristic = pService->createCharacteristic(
-        CHARACTERISTIC_UUID,
-        BLECharacteristic::PROPERTY_READ |
-            BLECharacteristic::PROPERTY_NOTIFY);
+    // BLEDevice::init("ESP32 Sensor Export");
+    // pServer = BLEDevice::createServer();
+    // BLEService *pService = pServer->createService(SERVICE_UUID);
+    // pCharacteristic = pService->createCharacteristic(
+    //     CHARACTERISTIC_UUID,
+    //     BLECharacteristic::PROPERTY_READ |
+    //         BLECharacteristic::PROPERTY_NOTIFY);
 
-    pCharacteristic->setCallbacks(new MyCallbacks());
+    // pCharacteristic->setCallbacks(new MyCallbacks());
 
-    // set initial sensor value
-    pCharacteristic->setValue(String(sensorValue).c_str());
+    // // set initial sensor value
+    // pCharacteristic->setValue(String(sensorValue).c_str());
 
-    pService->start();
+    // pService->start();
 
-    // start advertising
-    pServer->getAdvertising()->addServiceUUID(pService->getUUID());
-    pServer->getAdvertising()->start();
+    // // start advertising
+    // pServer->getAdvertising()->addServiceUUID(pService->getUUID());
+    // pServer->getAdvertising()->start();
 }
 
 void loop() {
@@ -159,76 +158,108 @@ void loop() {
   // // readMux(2): DIP
   // // readMux(3): Andere Drehachse, ...
    
-    for (int j=0;j<16;j++){
-    potentiometerValue = readMux(j);
-    // Serial.print(potentiometerValue);
-    array[j]=potentiometerValue;
-    }
+//     for (int j=0;j<16;j++){
+//     potentiometerValue = readMux(j);
+//     // Serial.print(potentiometerValue);
+//     array[j]=potentiometerValue;
+//     }
 
 
-  // // Kraftwerte auslesen, aktueller Widerstand: 10k Ohm
-    forceValue[0] = analogRead(force1); // Kraftsensor Zeigefinger
-    forceValue[1] = analogRead(force2); // Kraftsensor Mittelfinger
-    forceValue[2] = analogRead(force3); // Kraftsensor Ringfinger
-    forceValue[3] = analogRead(force4); // Kraftsensor kleiner Finger
-    for (int k = 0; k<4; k++){
-    array[16 + k]=forceValue[k];
-    //   Serial.print(forceValue[k]);
-    }
+//   // // Kraftwerte auslesen, aktueller Widerstand: 10k Ohm
+//     forceValue[0] = analogRead(force2); // Kraftsensor Zeigefinger
+//     forceValue[1] = analogRead(force3); // Kraftsensor Mittelfinger
+//     forceValue[2] = analogRead(force4); // Kraftsensor Ringfinger
+//     forceValue[3] = analogRead(force5); // Kraftsensor kleiner Finger
+//     for (int k = 0; k<4; k++){
+//     array[16 + k]=forceValue[k];
+//     //   Serial.print(forceValue[k]);
+//     }
+
+Serial.print("\nforce2: ");
+Serial.println(analogRead(force2));
+Serial.print("force3: ");
+Serial.println(analogRead(force3));
+Serial.print("force5: ");
+Serial.println(analogRead(force5));
 
 
-  //   Ansteuerung Servos über Multiplexer, bei 50 Hz sollte der Servo bei 375 (1500 / 4) still stehen
-    // myServos.setPWM(0, 0, 390);
-    // myServos.setPWM(4, 0, 390);
-    // myServos.setPWM(8, 0, 390);
+    myServos.setPWM(1, 0, 390);
+    myServos.setPWM(2, 0, 390);
+    myServos.setPWM(3, 0, 390);
+    myServos.setPWM(4, 0, 390);
+
+    delay(3000);
+    
+    myServos.setPWM(1, 0, 375);
+    myServos.setPWM(2, 0, 375);
+    myServos.setPWM(3, 0, 375);
+    myServos.setPWM(4, 0, 375);
+
+    delay(3000);
+
+    myServos.setPWM(1, 0, 360);
+    myServos.setPWM(2, 0, 360);
+    myServos.setPWM(3, 0, 360);
+    myServos.setPWM(4, 0, 360);
+
+    delay(3000);
+    
+    myServos.setPWM(1, 0, 375);
+    myServos.setPWM(2, 0, 375);
+    myServos.setPWM(3, 0, 375);
+    myServos.setPWM(4, 0, 375);
+
+    delay(3000);
+   
+    // int test=analogRead(force2);
+    // Serial.println(test);
+    // if (test<800){
+    //     myServos.setPWM(2, 0, 387);
+    // }
+    // else{
+    //     myServos.setPWM(2, 0, 363);
+    // }
+    // delay(500);
+
+  //   Ansteuerung Servos über Multiplexer, bei 50 Hz sollte der Servo bei 375 (1500 / 4) still stehen (+-11,25)
+    // myServos.setPWM(0, 0, 400);
+    // myServos.setPWM(2, 0, 400);
+    // myServos.setPWM(4, 0, 400);
+    // myServos.setPWM(8, 0, 400);
     // delay(1000);
 
     // myServos.setPWM(0, 0, 375);
+    // myServos.setPWM(2, 0, 375);
     // myServos.setPWM(4, 0, 375);
     // myServos.setPWM(8, 0, 375);
     // delay(2000);
 
-    // myServos.setPWM(0, 0, 360);
-    // myServos.setPWM(4, 0, 360);
-    // myServos.setPWM(8, 0, 360);
+    // myServos.setPWM(0, 0, 350);
+    // myServos.setPWM(2, 0, 350);
+    // myServos.setPWM(4, 0, 350);
+    // myServos.setPWM(8, 0, 350);
     // delay(1000);
 
     // myServos.setPWM(0, 0, 375);
+    // myServos.setPWM(2, 0, 375);
     // myServos.setPWM(4, 0, 375);
     // myServos.setPWM(8, 0, 375);
     // delay(2000);
-
+    
 
 // read IMU
     /* Get new sensor events with the readings */
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
+    // sensors_event_t a, g, temp;
+    // mpu.getEvent(&a, &g, &temp);
 
     // save sensor values to array
-    array[20]= a.acceleration.x;
-    array[21]= a.acceleration.y;
-    array[22]= a.acceleration.z;
-    array[23]= g.gyro.x;
-    array[24]= g.gyro.y;
-    array[25]= g.gyro.z;
-    /* Print out the values */
-    //   Serial.print("Acceleration X: ");
-    //   Serial.print(a.acceleration.x);
-    //   Serial.print(", Y: ");
-    //   Serial.print(a.acceleration.y);
-    //   Serial.print(", Z: ");
-    //   Serial.print(a.acceleration.z);
-    //   Serial.println(" m/s^2");
-    //   Serial.print("Rotation X: ");
-    //   Serial.print(g.gyro.x);
-    //   Serial.print(", Y: ");
-    //   Serial.print(g.gyro.y);
-    //   Serial.print(", Z: ");
-    //   Serial.print(g.gyro.z);
-    //   Serial.println(" rad/s");
-    //   Serial.print("Temperature: ");
-    //   Serial.print(temp.temperature);
-    //   Serial.println(" degC");
+    // Serial.println(a.acceleration.y);
+    // array[20]= a.acceleration.x;
+    // array[21]= a.acceleration.y;
+    // array[22]= a.acceleration.z;
+    // array[23]= g.gyro.x;
+    // array[24]= g.gyro.y;
+    // array[25]= g.gyro.z;
 
 
     // print array values on serial monitor
@@ -240,19 +271,21 @@ void loop() {
     //     }
     
     
-    // Bluetooth Server
-    String arrayData = "";
-    for (int z = 0; z < 26; z++) {
-      arrayData += String(array[z]) + ",";
-    }
+    // // Bluetooth Server
+    // String arrayData = "";
+    // for (int z = 0; z < 26; z++) {
+    //   arrayData += String(array[z]) + ",";
+    // }
 
-    // update characteristic value
-    pCharacteristic->setValue(arrayData.c_str());
+    // // update characteristic value
+    // pCharacteristic->setValue(arrayData.c_str());
 
-    // notify connected clients
-    pCharacteristic->notify();
+    // // notify connected clients
+    // pCharacteristic->notify();
 
-    delay(3000);
+    // Serial.println(readMux(2));
+
+    // delay(3000);
 
 //     count++;
 //   }
