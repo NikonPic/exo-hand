e71a60169e41a6be3c874863d8d3ba611d6a4da3
annickls <129967947+annickls@users.noreply.github.com>
2023-06-23 16:59:56 +0200

    Regelung überarbeitet

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 
+
 // #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 // #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
@@ -37,27 +38,22 @@ int forceValue[4] = {0, 0, 0, 0};
 int sensorValue = 0;
 unsigned long startTime = 0;
 int count = 0;
-float array[20];
+float sensor_array[20];
 // Kraftregelung
-int dir_2 = 0;
-int dir_3 = 0;
-int dir_4 = 0;
-int dir_5 = 1;
+int dir_array[4] = {0, 0, 0, 0};
 int F_upper = 2500;
 int F_lower = -500;
 int angle_2 = 0;
 int angle_3 = 0;
 int angle_4 = 0;
+// Zeit
+int Startzeit = 0;
+int Dauer = 10000;
+
 
 // Servos Ansteuerung Multiplexer
 Adafruit_PWMServoDriver myServos = Adafruit_PWMServoDriver(0x40);
 
-
-// Skalierung des gemessenen Signals auf die Spannung (für Potentiometer)
-float floatMap(float x, float in_min, float in_max, float out_min, float out_max)
-{
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
 
 // logische Schaltung des Multiplexers (Potentiometer)
 int readMux(int channel)
@@ -93,6 +89,36 @@ int readMux(int channel)
     float valGrad = map(val, 0, 4095, 0, 360); // Wert in Winkel [°] umrechnen (Annahme: Winkelbereich Poti 360°)
     return valGrad;                            // Wert zurück geben
 }
+
+
+// Motor Regelung
+    // Ansteuerung Servos über Multiplexer, bei 50 Hz sollte der Servo bei 375 (1500 / 4) still stehen (+-11,25)
+    // Regelung der Servos: swich case für jeden Finger, jeweils obere und untere Kraftgrenze definiert Richtung des Servos (dir)
+    // Geschwindigkeit: Je größer die Differenz zum Grenzwert, desto höher die Geschwindkeit, bei Annäherung Verringerung
+void control (int pin, int dir, int force)
+{
+    switch (dir){
+        case 0:
+            if (force < 1800){
+            myServos.setPWM(pin, 0, 395 - (force-1000)/100);
+            delay(20);
+            }
+            else{
+                dir_array[pin] = 1;
+            }
+        break;
+        case 1:
+            if (force>1500){
+            myServos.setPWM(pin, 0, 365 - (force-1000)/100);
+            delay(20);
+            }
+            else{
+                dir_array[pin]=0;
+            }
+        break;
+        }
+}
+
 
 void setup() {
 
@@ -135,114 +161,78 @@ void setup() {
     // // start advertising
     // pServer->getAdvertising()->addServiceUUID(pService->getUUID());
     // pServer->getAdvertising()->start();
+
+
+Startzeit=millis();
 }
 
 void loop() {
 
-  // Potentiometerwerte einlesen
-    // readMux(0) - readMux(3) = Zeigefinger
-    // readMux(0): MCP
-    // readMux(1): PIP
-    // readMux(2): DIP
-    // readMux(3): Andere Drehachse, ...
-   
-    for (int j=0;j<16;j++){
-    potentiometerValue = readMux(j);
-    array[j]=potentiometerValue;
-    }
+if (millis() - Startzeit <= Dauer){
+
+    // Potentiometerwerte einlesen
+        
+        // readMux(0) - readMux(3) = Zeigefinger
+        // readMux(0): MCP
+        // readMux(1): PIP
+        // readMux(2): DIP
+        // readMux(3): Andere Drehachse, ...
+    
+        for (int j=0;j<16;j++){
+        potentiometerValue = readMux(j);
+        sensor_array[j]=potentiometerValue;
+        Serial.print(sensor_array[j]);
+        Serial.print(",");
+        }
+
+    
+        // Offset der Winkel abziehen
+        angle_2 = sensor_array[0] - 73;
+        angle_3 = sensor_array[4] - 73;
+        angle_4 = sensor_array[8] - 73;
+
 
     // Kraftwerte einlesen, Widerstand Spannungsteiler: 10k Ohm
-    array[16] = analogRead(force2); // Kraftsensor Zeigefinger
-    array[17] = analogRead(force3); // Kraftsensor Mittelfinger
-    array[18] = analogRead(force4); // Kraftsensor Ringfinger
-    array[19] = analogRead(force5); // Kraftsensor kleiner Finger
+
+        sensor_array[16] = analogRead(force2); // Kraftsensor Zeigefinger
+        sensor_array[17] = analogRead(force3); // Kraftsensor Mittelfinger
+        sensor_array[18] = analogRead(force4); // Kraftsensor Ringfinger
+        sensor_array[19] = analogRead(force5); // Kraftsensor kleiner Finger
     
-    // Offset der Winkel abziehen
-    angle_2 = array[0] - 73;
-    angle_3 = array[4] - 73;
-    angle_4 = array[8] - 73;
+        Serial.print(sensor_array[16]);
+        Serial.print(",");
+        Serial.print(sensor_array[17]);
+        Serial.print(",");
+        Serial.print(sensor_array[18]);
+        Serial.print(",");
+        Serial.print(sensor_array[19]);
+        Serial.print("\n");
+    
+    // Ansteuerung Servos: dir_array[0] = Zeigefinger, dir_array[1] = Mittelfinger, ...
 
-
-    // Ansteuerung Servos über Multiplexer, bei 50 Hz sollte der Servo bei 375 (1500 / 4) still stehen (+-11,25)
-    // Regelung der Servos: Ein swicht case für jeden Finger, jeweils obere und untere Grenze definiert Richtung des Servos
-    // Geschwindigkeit: Je größer Differenz zum Grenzwert, desto höher Geschwindkeit, bei Annäherung Verringerung
-   
-
-    switch (dir_2){
-        case 0:
-        if (angle_2 < 50){
-        myServos.setPWM(0, 0, 390 - angle_2/10);
-        delay(20);
-        }
-        else{
-            dir_2 = 1;
-        }
-        break;
-        case 1:
-        if (angle_2>10){
-        myServos.setPWM(0, 0, 365 - angle_2/10);
-        delay(20);
-        }
-        else{
-            dir_2=0;
-        }
-        break;
-    }
+        control(0, dir_array[0], sensor_array[16]);
+        // control(1, dir_array[1], sensor_array[17]);
+        // control(2, dir_array[2], sensor_array[18]);
+        // control(4, dir_array[3], sensor_array[19]);
         
-    switch (dir_3){
-        case 0:
-        if (angle_3 < 60){
-        myServos.setPWM(1, 0, 390 - angle_3/10);
-        delay(20);
-        }
-        else{
-            dir_3 = 1;
-        }
-        break;
-        case 1:
-        if (angle_3>20){
-        myServos.setPWM(1, 0, 365 - angle_3/10);
-        delay(20);
-        }
-        else{
-            dir_3=0;
-        }
-        break;
-    }
-
-    switch (dir_4){
-        case 0:
-        if (angle_4 < 60){
-        myServos.setPWM(2, 0, 390 - angle_4/10);
-        delay(20);
-        }
-        else{
-            dir_4 = 1;
-        }
-        break;
-        case 1:
-        if (angle_4>20){
-        myServos.setPWM(2, 0, 365 - angle_4/10);
-        delay(20);
-        }
-        else{
-            dir_4 = 0;
-        }
-        break;
-    }
-    
 
     
+
     // // Bluetooth Übertragung Server
-    // String arrayData = "";
-    // for (int z = 0; z < 20; z++) {
-    //   arrayData += String(array[z]) + ",";
-    // }
 
-    // // update characteristic value
-    // pCharacteristic->setValue(arrayData.c_str());
+        // String arrayData = "";
+        // for (int z = 0; z < 20; z++) {
+        //   arrayData += String(sensor_array[z]) + ",";
+        // }
 
-    // // notify connected clients
-    // pCharacteristic->notify();
+        // // update characteristic value
+        // pCharacteristic->setValue(arrayData.c_str());
+
+        // // notify connected clients
+        // pCharacteristic->notify();
+}
+else{
+    while(1);
+    }
   
 }
